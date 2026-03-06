5dbf2b9b0c86f3f68193dd3b673f1309641aa403
unknown <lilaharyanti45@gmail.com>
2026-03-06 13:51:20 +0700

    update dashboard

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -11,6 +11,17 @@ class Author extends Model
 
     protected $guarded = ['id'];
 
+/*************  ✨ Windsurf Command ⭐  *************/
+    /**
+     * Get the books written by this author.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+/*******  647da365-a9f4-470b-b859-690d3069a147  *******/    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
     public function book()
     {
         return $this->hasMany(Book::class);
